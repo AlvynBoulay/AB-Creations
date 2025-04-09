869b68a55fea2c1330c52edd521d2770fb1ded12
AlvBou <34zdraw@gmail.com>
2025-04-09 16:02:50 +0200

    init - Header et Menu de navigation en CSS

---
+++ js/app.js
@@ -0,0 +1,11 @@
+let btnCloseMainNav = document.querySelector('.closeMainNav');
+
+btnCloseMainNav.addEventListener('click', function() {
+    document.body.classList.remove('has-menu-opened')
+});
+
+let btnOpenMainNav = document.querySelector('.openMainNav');
+
+btnOpenMainNav.addEventListener('click', function() {
+    document.body.classList.add('has-menu-opened')
+});
